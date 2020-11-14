61c59e11460feea2800010d8a156f51a70d9ae26
Flávio Sousa <root@fjsousa.com>
2020-11-14 16:34:31 +0000

    close modal

--- src/blog/frontend/main.cljs
+++ src/blog/frontend/main.cljs
@@ -4,19 +4,28 @@
 ;;load script as
 ;;["/js/file.js" :as dep]
 
+(defn open-modal-evl [el]
+  (fn [_]
+    (let [i (.getAttribute el "data-lightbox")
+          data-lightbox (.querySelector js/document (str ".lightbox[data-lightbox=\"" i "\"]") )]
+      (.add (.-classList data-lightbox ) "open"))))
+
+(defn close-modal-evl [el]
+  (fn [_]
+    (-> el .-parentElement .-parentElement .-classList (.remove "open"))
+    (js/console.log el)))
+
+(defn init-grid []
+  (let [grid-elem (.querySelector js/document ".masonry-grid")
+        lightboxes (.getElementsByClassName js/document "open-lightbox")
+        close-elem (.getElementsByClassName js/document "close")]
+    (new masonry grid-elem #js {:itemSelector ".masonry-item", :fitWidth true, :gutter 30})
+    (-> lightboxes js/Array.from (.forEach (fn [el]
+                                             (.addEventListener el "click" (open-modal-evl el)))))
+    (-> close-elem js/Array.from (.forEach (fn [el]
+                                             (.addEventListener el "click" (close-modal-evl el)))))))
+
+
 (defn ^:export init []
-  (.initHighlightingOnLoad hljs))
-
-(def grid-elem (.querySelector js/document ".masonry-grid"))
-
-(def grid-masonry (new masonry grid-elem #js {:itemSelector ".masonry-item", :fitWidth true, :gutter 30}))
-
-(.querySelector js/document ".open-lightbox")
-
-(.forEach (js/Array.from (.getElementsByClassName js/document "open-lightbox"))
-          (fn [el]
-            (.addEventListener el "click" (fn [_]
-                                            (let [lightbox-nr (.getAttribute el "data-lightbox")
-                                                  data-lightbox (.querySelector js/document (str ".lightbox[data-lightbox=\"" lightbox-nr "\"]") )]
-                                              (.add (.-classList data-lightbox ) "open")
-                                              )))))
+  (.initHighlightingOnLoad hljs)
+  (init-grid))
