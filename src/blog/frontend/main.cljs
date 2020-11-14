e6a6b73c6e55d92fcca6ca175cd907d4b40f44db
Flávio Sousa <root@fjsousa.com>
2020-11-14 17:47:27 +0000

    add event listeners

--- src/blog/frontend/main.cljs
+++ src/blog/frontend/main.cljs
@@ -12,9 +12,17 @@
 
 (defn close-modal-evl [el]
   (fn [_]
-    (-> el .-parentElement .-parentElement .-classList (.remove "open"))
-    (js/console.log el)))
+    (-> el .-parentElement .-parentElement .-classList (.remove "open"))))
 
+(defn close-modal-click-outside-evl [ev]
+  (let [modal-el (.querySelector js/document ".inner")
+        is-outside-modal? (not (.contains modal-el (.-target ev)))
+        modal-open? (not (nil? (.querySelector js/document ".lightbox.open *")))]
+    (when (and modal-open? is-outside-modal?)
+      (-> (.getElementsByClassName js/document "lightbox") js/Array.from (.forEach (fn [el]
+                                                                                     ;;true means the event is handled by this event handler
+                                                                                     ;;before being passed to the elements below
+                                                                                     (-> el .-classList (.remove "open"))))))))
 (defn init-grid []
   (let [grid-elem (.querySelector js/document ".masonry-grid")
         lightboxes (.getElementsByClassName js/document "open-lightbox")
@@ -23,7 +31,9 @@
     (-> lightboxes js/Array.from (.forEach (fn [el]
                                              (.addEventListener el "click" (open-modal-evl el)))))
     (-> close-elem js/Array.from (.forEach (fn [el]
-                                             (.addEventListener el "click" (close-modal-evl el)))))))
+                                             (.addEventListener el "click" (close-modal-evl el)))))
+    (.addEventListener js/document "click" close-modal-click-outside-evl true)))
+
 
 
 (defn ^:export init []
