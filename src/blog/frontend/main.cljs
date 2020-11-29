9764ed27eb13968fb1cbeac1cd0fa7456565efdd
Flávio Sousa <root@fjsousa.com>
2020-11-29 11:20:37 +0000

    scss

--- src/blog/frontend/main.cljs
+++ src/blog/frontend/main.cljs
@@ -5,10 +5,10 @@
 ;;["/js/file.js" :as dep]
 
 (defn open-modal-evl [el]
-  (fn [_]
-    (let [i (.getAttribute el "data-lightbox")
-          data-lightbox (.querySelector js/document (str ".lightbox[data-lightbox=\"" i "\"]") )]
-      (.add (.-classList data-lightbox ) "open"))))
+            (fn [_]
+              (let [i (.getAttribute el "data-lightbox")
+                    data-lightbox (.querySelector js/document (str ".lightbox[data-lightbox=\"" i "\"]") )]
+                (.add (.-classList data-lightbox ) "open"))))
 
 (defn close-modal-evl [el]
   (fn [_]
@@ -27,7 +27,7 @@
   (let [grid-elem (.querySelector js/document ".masonry-grid")
         lightboxes (.getElementsByClassName js/document "open-lightbox")
         close-elem (.getElementsByClassName js/document "close")]
-    (new masonry grid-elem #js {:itemSelector ".masonry-item", :fitWidth true, :gutter 30})
+    #_(new masonry grid-elem #js {:itemSelector ".masonry-item", :fitWidth true, :gutter 30})
     (-> lightboxes js/Array.from (.forEach (fn [el]
                                              (.addEventListener el "click" (open-modal-evl el)))))
     (-> close-elem js/Array.from (.forEach (fn [el]
